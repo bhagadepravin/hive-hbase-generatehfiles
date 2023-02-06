1cbe0405566663d23ca5a9ce2bf5b8b06d87bba7
Pravin Bhagade <pbhagade@hortonworks.com>
2023-02-06 20:47:32 +0530

    Update 03-create_orc_table.sh

--- 03-create_orc_table.sh
+++ 03-create_orc_table.sh
@@ -11,10 +11,10 @@ HIVE_TABLE_NAME=passwd_orc
 
 # Create the hive table
 echo -e "\nRunning hive script $HIVE_SCRIPT"
-hive -S -f $HIVE_SCRIPT 2>/dev/null
+hive -f $HIVE_SCRIPT 2>/dev/null
 
 # Run a select to validate
 echo -e "\nRunning select on table $HIVE_TABLE_NAME"
-hive -S -e "select * from $HIVE_TABLE_NAME limit 10;" 2>/dev/null
+hive -e "select * from $HIVE_TABLE_NAME limit 10;" 2>/dev/null
 
 exit 0
