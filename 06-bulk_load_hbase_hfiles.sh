9d5b7df9fb6e2af5928a8a99f5737441f888b2dd
Pravin Bhagade <pbhagade@hortonworks.com>
2023-02-08 16:03:53 +0530

    Update 06-bulk_load_hbase_hfiles.sh

--- 06-bulk_load_hbase_hfiles.sh
+++ 06-bulk_load_hbase_hfiles.sh
@@ -12,8 +12,9 @@ HFILE_HDFS_INPUT_DIR=/tmp/hive_hfile_test/passwd_hbase_hfiles
 # Create the hive table
 echo -e "\nRunning the hbase bulk load for $HBASE_TABLE_NAME using input at $HFILE_HDFS_INPUT_DIR"
 export HADOOP_CLASSPATH=`hbase classpath`
-yarn jar /usr/hdp/current/hbase-client/lib/hbase-server.jar completebulkload $HFILE_HDFS_INPUT_DIR $HBASE_TABLE_NAME
+#yarn jar /usr/hdp/current/hbase-client/lib/hbase-server.jar completebulkload $HFILE_HDFS_INPUT_DIR $HBASE_TABLE_NAME
+yarn jar /usr/hdp/current/hbase-client/lib/hbase-mapreduce.jar completebulkload $HFILE_HDFS_INPUT_DIR $HBASE_TABLE_NAME
 
 # Run a select to validate
 echo -e "\nRunning select on table $HIVE_TABLE_NAME"
-hive -S -e "select * from $HIVE_TABLE_NAME limit 10;" 2>/dev/null
+hive -e "select * from $HIVE_TABLE_NAME limit 10;" 2>/dev/null
